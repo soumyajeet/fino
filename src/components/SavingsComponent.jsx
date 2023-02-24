7e34524ac1598e6a35aee6073f79e4acabec13ce
soumyajeet <sendtosoumyajit@gmail.com>
2023-02-24 23:34:37 +0530

    savings component modified

--- src/components/SavingsComponent.jsx
+++ src/components/SavingsComponent.jsx
@@ -5,6 +5,14 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import TextField from '@mui/material/TextField';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
+import SaveIcon from '@mui/icons-material/Save';
 import Snackbar from '@mui/material/Snackbar';
 import { styled } from '@mui/material/styles';
 import FormGroup from '@mui/material/FormGroup';
@@ -21,15 +29,95 @@ import { Progress } from 'react-sweet-progress';
 import "react-sweet-progress/lib/style.css";
 
 
-export default function SavingsComponent({savingsValue}) {
+export default function SavingsComponent({ savingsValue }) {
+    const [formValues, setFormValues] = useState([{ itemName: "", itemBudget: "" }]);
+
+    let handleChange = (i, e) => {
+        console.log(i)
+        console.log(e.target.value)
+        let newFormValues = [...formValues];
+        newFormValues[i][e.target.name] = e.target.value;
+        setFormValues(newFormValues);
+    }
+
+    let addFormFields = () => {
+        console.log("Clicked")
+        setFormValues([...formValues, { itemName: "", itemBudget: "" }])
+    }
+
+    let removeFormFields = (i) => {
+        let newFormValues = [...formValues];
+        newFormValues.splice(i, 1);
+        setFormValues(newFormValues)
+    }
+
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        let sum = 0;
+        formValues.forEach((item, i) => {
+            sum = sum + parseInt(item.itemBudget);
+            if (sum >= savingsValue) {
+                console.log("Your Save amount is lesser than you added!");
+            } else {
+                alert(JSON.stringify(formValues));
+            }
+        })
+    }
     return (
         <>
-            <Box sx={{ minWidth: 275 }}>
+            <Box sx={{ minWidth: 375 }}>
                 <Card variant="outlined">
                     <CardContent>
                         <Typography variant="h6" component="h6" sx={{ p: 2, color: 'primary.main' }}>
                             Savings {savingsValue}
                         </Typography>
+                        <Stack spacing={3} sx={{ p: 2 }}>
+                            <form onSubmit={handleSubmit}>
+                                <div className="button-section">
+                                    <Tooltip title="Add New Row">
+                                        <IconButton color="warning" onClick={() => addFormFields()}>
+                                            <AddIcon />
+                                        </IconButton>
+                                    </Tooltip>
+                                    <Tooltip title="Save Items">
+                                        <IconButton variant="contained" color="success" type="submit">
+                                            <SaveIcon />
+                                        </IconButton>
+                                    </Tooltip>
+
+                                </div>
+                                {formValues.map((elem, index) => {
+                                    return (
+                                        <Box key={index} sx={{ mt: 2, mb: 2 }}>
+                                            <Select
+                                                labelId="item-name"
+                                                id="item-name-save"
+                                                value={elem.itemName}
+                                                label="Item"
+                                                name="itemName"
+                                                onChange={e => handleChange(index, e)}
+                                                sx={{ m: 2 }}
+                                            >
+                                                <MenuItem value={'LIC'}>LIC</MenuItem>
+                                                <MenuItem value={'NPS'}>NPS</MenuItem>
+                                                <MenuItem value={'PPF'}>PPF</MenuItem>
+                                            </Select>
+                                            <TextField name="itemBudget" value={elem.itemBudget || ""} onChange={e => handleChange(index, e)} label="Budget" sx={{ mt: 2, mb: 2 }} />
+                                            {
+                                                index ?
+                                                    <Tooltip title="Delete Item">
+                                                        <IconButton color="error" onClick={() => removeFormFields(index)} sx={{ m: 2 }}>
+                                                            <DeleteIcon />
+                                                        </IconButton>
+                                                    </Tooltip>
+                                                    : null
+                                            }
+                                        </Box>
+                                    )
+                                })}
+
+                            </form>
+                        </Stack>
                     </CardContent>
                 </Card>
             </Box>
--- src/components/WantComponent.jsx
+++ src/components/WantComponent.jsx
@@ -15,20 +15,19 @@ import { saveData } from './slice/needSlice';
 
 
 function WantComponent({ valuation }) {
-    const [formValues, setFormValues] = useState([{ itemName: "", itemBudget: "" }])
-
+    const [formValues, setFormValues] = useState([{ itemName: "", itemBudget: "" }]);    
 
     let handleChange = (i, e) => {
         let newFormValues = [...formValues];
         newFormValues[i][e.target.name] = e.target.value;
         setFormValues(newFormValues);
-     }
-        
+    }
+
     let addFormFields = () => {
         console.log("Clicked")
         setFormValues([...formValues, { itemName: "", itemBudget: "" }])
-     }
-    
+    }
+
     let removeFormFields = (i) => {
         let newFormValues = [...formValues];
         newFormValues.splice(i, 1);
@@ -37,7 +36,15 @@ function WantComponent({ valuation }) {
 
     let handleSubmit = (event) => {
         event.preventDefault();
-        alert(JSON.stringify(formValues));
+        let sum = 0;
+        formValues.forEach((item, i)=> {
+            sum  = sum + parseInt(item.itemBudget);
+            if(sum >= valuation) {
+                console.log("Your want amount is lesser than you added!");
+            } else {
+                alert(JSON.stringify(formValues));
+            }
+        })
     }
 
 
@@ -48,25 +55,27 @@ function WantComponent({ valuation }) {
                     <Typography variant="h6" component="h6" sx={{ p: 2, color: 'warning.main' }}>
                         Want {valuation}
                     </Typography>
-                    <form onSubmit={handleSubmit}>
-                        {formValues.map((elem, index) => {
-                            return (
-                                <Box key={index} sx={{ m: 2 }}>
-                                    <TextField name="itemName" value={elem.itemName || ""} onChange={e => handleChange(index, e)} label="Item" sx={{ m: 2 }}/>
-                                    <TextField name="itemBudget" value={elem.itemBudget || ""} onChange={e => handleChange(index, e)} label="Budget" sx={{ m: 2 }}/>
-                                    {
-                                        index ?
-                                            <Button variant="contained" color="error" onClick={() => removeFormFields(index)} sx={{ m: 2 }}>X</Button>
-                                            : null
-                                    }
-                                </Box>
-                            )
-                        })}
-                        <div className="button-section">
-                            <Button variant="contained" color="warning" onClick={() => addFormFields()} sx={{ m: 2 }}>Add</Button>
-                            <Button variant="contained" color="success" type="submit" sx={{ m: 2 }}>Submit</Button>
-                        </div>
-                    </form>
+                    <Stack spacing={3} sx={{ p: 2 }}>
+                        <form onSubmit={handleSubmit}>
+                            {formValues.map((elem, index) => {
+                                return (
+                                    <Box key={index} sx={{ m: 2 }}>
+                                        <TextField name="itemName" value={elem.itemName || ""} onChange={e => handleChange(index, e)} label="Item" sx={{ m: 2 }} />
+                                        <TextField name="itemBudget" value={elem.itemBudget || ""} onChange={e => handleChange(index, e)} label="Budget" sx={{ m: 2 }} />
+                                        {
+                                            index ?
+                                                <Button variant="contained" color="error" onClick={() => removeFormFields(index)} sx={{ m: 2 }}>X</Button>
+                                                : null
+                                        }
+                                    </Box>
+                                )
+                            })}
+                            <div className="button-section">
+                                <Button variant="contained" color="warning" onClick={() => addFormFields()} sx={{ m: 2 }}>Add</Button>
+                                <Button variant="contained" color="success" type="submit" sx={{ m: 2 }}>Submit</Button>
+                            </div>
+                        </form>
+                    </Stack>
                 </CardContent>
             </Card>
         </Box>
